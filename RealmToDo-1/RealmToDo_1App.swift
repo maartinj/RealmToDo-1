61a7a0a521df53b5a258ca558482ddfa3dc5838e
Marcin Jędrzejak <53284922+maartinj@users.noreply.github.com>
2023-05-13 09:43:14 +0200

    first commit: Load initial files

---
+++ RealmToDo-1/RealmToDo_1App.swift
@@ -0,0 +1,17 @@
+//
+//  RealmToDo_1App.swift
+//  RealmToDo-1
+//
+//  Created by Marcin Jędrzejak on 13/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct RealmToDo_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
